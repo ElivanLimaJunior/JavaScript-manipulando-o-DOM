d03b57891283690ce133d0305423d9d9d41e0cd7
Elivan-Jr <elivanlimajunior@gmail.com>
2022-07-06 18:07:44 -0300

    Funções
    Vimos um pouco de Funções anônimas e usamos a arrow function.
    
    Também acrescentamos no nosso código uma maneira de chamar a pessoa pelo nome.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 const robotron = document.querySelector('#robotron')
 
-robotron.addEventListener("click", dizoi)
+robotron.addEventListener("click", () => {
+    console.log('Oi')
+})
 
-function dizoi() {
-    console.log('oi')
+function dizoi(nome) { // colocamos nome dentro do parametros e no console chamamos esse nome, mas de onde vem esse nome ?
+    console.log('oi ' + nome)
+    console.log('Bem-vindo ao robotron')
 }
+
+dizoi("pedro") // esse nome vem quando chamamos a função, ali nós definimos qual seria o valor do parametro nome.
