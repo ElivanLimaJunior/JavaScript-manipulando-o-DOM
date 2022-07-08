3fde29355fe26c7077a19d7d925aced2269365c3
Elivan-Jr <elivanlimajunior@gmail.com>
2022-07-08 19:37:47 -0300

    Usando o forEach
    Usamos o forEach para percorrer todos os elementos com a classe: ".controle-ajuste".
     Usamos o console.log("funcionou") dentro da função anonima primeiro, para verificar se o click nos demais botões estavam sendo percorridos também.
    
    Após essa verificação, pegamos o target do evento e colocamos em outro console.log testar + uma vez: "console.log(evento.target)" . Lembrando que tudo isso é dentro da função anônima.
    
    Após está tudo certinho avançamos.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-const subtrair = document.querySelector('#subtrair')
 const somar = document.querySelector('#somar')
+const subtrair = document.querySelector('#subtrair')
 const braco = document.querySelector('#braco')
+
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        // Primeiro usamos o console.log(evento) para analisar o target do click. depois disso usamos o "evento.target" para pegar exatamento o botão em que estamos clicando.
+        console.log(evento.target) 
+    })
+})
 
 somar.addEventListener("click", () => {manipulaDados("somar")})
 
