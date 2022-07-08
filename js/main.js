9afd79666be302ba7b1c700a0de2c3c984eb5468
Elivan-Jr <elivanlimajunior@gmail.com>
2022-07-08 20:18:57 -0300

    Fim da aula 3
    Nessa aula:
    
    Definimos o que é Array e como manipular seus objetos;
    
    Como utilizar arrow functions;
    
    Como utilizar o método splice() para remover itens.

--- js/main.js
+++ js/main.js
@@ -11,12 +11,6 @@ controle.forEach( (elemento) => {
     })
 })
 
-// Não é + necessario essa parte do código pois o código acima fara o mesmo porém para todos.
-
-// somar.addEventListener("click", () => {manipulaDados("somar")})
-
-// subtrair.addEventListener("click", () => {manipulaDados("subtrair")})
-
 function manipulaDados (operacao) {
     if(operacao === "-") {
         braco.value = parseInt(braco.value) - 1
